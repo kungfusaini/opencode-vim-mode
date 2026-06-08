3b85ad9d83a8f7a20882c5501eb49c119c11abae
sumeet <sumeetsaini621@gmail.com>
2026-06-08 17:33:41 +0100

    Expose TUI plugin entrypoint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import indicator from "./indicator.js"
 import vimMode from "./vim-mode.js"
 
+export const id = "opencode-vim-mode"
+
+export async function tui(api) {
+  await indicator.tui(api)
+  await vimMode.tui(api)
+}
+
 export default {
-  id: "opencode-vim-mode",
-  async tui(api) {
-    await indicator.tui(api)
-    await vimMode.tui(api)
-  },
+  id,
+  tui,
 }
